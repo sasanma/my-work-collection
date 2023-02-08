f9a142611837226a078a82ba2ce5da5e3c21fcb9
sasanma <81403961+sasanma@users.noreply.github.com>
2023-02-09 02:37:52 +0900

    ポップアップ表示時のアニメーションを追加

--- picture.js
+++ picture.js
@@ -1,17 +1,31 @@
+var img;    //選択画像
+var id;     //setTimeoutのid
+
 $(".card").click(function() {
-    $(".attention").css({
-        visibility: "visible"
-    });
-    $(this).children("img").clone().prependTo(".popup").css({
+    img = $(this).children("img").clone().prependTo(".popup").css({
+        opacity: "0",
         position: "relative",
         width: "var(--popup-width)",
         height: "auto"
     });
+    img.stop(false, false);
+    $(".button").children("a").stop(false, false);
+    $(".button").children("a").css({opacity: "0"});
+    $(".attention").css({
+        visibility: "visible"
+    });
+    $(".popup").addClass("fadein");
+    id = setTimeout(function() { 
+        img.animate({opacity: "1"}, 1000);
+        $(".button").children("a").animate({opacity: "1"}, 1000);
+    }, 500);
 });
 
 $(".attention").click(function() {
+    clearTimeout(id);
+    img.remove();
+    $(".popup").removeClass("fadein");
     $(".attention").css({
         visibility: "hidden"
     });
-    $(".popup").children("img").remove()
 });
