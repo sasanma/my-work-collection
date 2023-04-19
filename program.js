b29040ce794b2655e05454fe16951698569a03f2
sasanma <81403961+sasanma@users.noreply.github.com>
2023-04-19 23:52:03 +0900

    インデントをタブに変更

--- program.js
+++ program.js
@@ -1,32 +1,32 @@
-var img;    //選択画像
-var id;     //setTimeoutのid
+var img; //選択画像
+var id; //setTimeoutのid
 
-$(".card").click(function() {
-    img = $(this).children("img").clone().prependTo(".popup").css({
-        opacity: "0",
-        position: "relative",
-        width: "var(--popup-width)",
-        height: "auto",
-        "max-height": "50vh"
-    });
-    img.stop(false, false);
-    $(".button").children("a").stop(false, false);
-    $(".button").children("a").css({opacity:"0"});
-    $(".attention").css({
-        visibility: "visible"
-    });
-    $(".popup").addClass("fadein");
-    id = setTimeout(function() { 
-        img.animate({opacity: "1"}, 1000);
-        $(".button").children("a").animate({opacity: "1"}, 1000);
-    }, 500);
+$(".card").click(function () {
+	img = $(this).children("img").clone().prependTo(".popup").css({
+		opacity: "0",
+		position: "relative",
+		width: "var(--popup-width)",
+		height: "auto",
+		"max-height": "50vh",
+	});
+	img.stop(false, false);
+	$(".button").children("a").stop(false, false);
+	$(".button").children("a").css({ opacity: "0" });
+	$(".attention").css({
+		visibility: "visible",
+	});
+	$(".popup").addClass("fadein");
+	id = setTimeout(function () {
+		img.animate({ opacity: "1" }, 1000);
+		$(".button").children("a").animate({ opacity: "1" }, 1000);
+	}, 500);
 });
 
-$(".attention").click(function() {
-    clearTimeout(id);
-    img.remove();
-    $(".popup").removeClass("fadein");
-    $(".attention").css({
-        visibility: "hidden"
-    });
+$(".attention").click(function () {
+	clearTimeout(id);
+	img.remove();
+	$(".popup").removeClass("fadein");
+	$(".attention").css({
+		visibility: "hidden",
+	});
 });
